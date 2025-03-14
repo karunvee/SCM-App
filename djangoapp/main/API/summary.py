e3fc03bc399a6e1607337aa913092ee6cff41a23
karunvee <vee488@hotmail.com>
2025-03-14 13:02:52 +0700

    analysis_breakdown dashboard

--- djangoapp/main/API/summary.py
+++ djangoapp/main/API/summary.py
@@ -31,31 +31,45 @@ def data_analysis_breakdown(request):
         machine_types = request.data.get('machine_types',[])
         equipments = request.data.get('equipments',[])
         date_mode = request.data.get('date_mode').lower()
-        date_range = request.data.get('date_range')
+        date_start = request.data.get('date_start')
+        # date_range = request.data.get('date_range')
+        # start_date =  datetime.strptime(date_range['start'], '%m/%d/%Y').date()
+        # end_date =  datetime.strptime(date_range['end'], '%m/%d/%Y').date()
 
-        start_date =  datetime.strptime(date_range['start'], '%m/%d/%Y').date()
-        end_date =  datetime.strptime(date_range['end'], '%m/%d/%Y').date()
+        
+        date_start =  datetime.strptime(date_start, '%m/%d/%Y').date() 
 
         if date_mode == 'days':
-            return Response({"detail": 'This mode is not available to use.'}, status=status.HTTP_403_FORBIDDEN)
+            date_end =  date_start + relativedelta(days=1)
+
+            return Response({"detail": 'This mode is not available to use.', "data": []}, status=status.HTTP_403_FORBIDDEN)
         
         elif date_mode == 'months':
+            date_end =  date_start + relativedelta(months=1)
+            print(date_start, date_end)
 
-            hObj = HistoryTrading.objects.filter(
-                Q(lines__line_name=line_name) &\
-                Q(component__location__name__in=locations) &\
-                Q(component__component_type__name__in=component_types) &\
-                Q(component__machine_type__name__in=machine_types) &\
-                Q(issue_date__month__range=[start_date.month, end_date.month])
-            )
-            print(hObj.values_list('component_id', flat=True))
-            print('*****')
+            # hObj = HistoryTrading.objects.filter(
+            #     Q(lines__line_name=line_name) &\
+            #     Q(component__location__name__in=locations) &\
+            #     Q(component__component_type__name__in=component_types) &\
+            #     Q(component__machine_type__name__in=machine_types) &\
+            #     Q(issue_date__date__range=[date_start, date_end])
+            # )
+            # print(hObj.values_list('component_id', flat=True))
+            # print('*****')
+            # components = Component.objects.filter(
+            #     id__in=hObj.values_list('component_id', flat=True)
+
             components = Component.objects.filter(
-                id__in=hObj.values_list('component_id', flat=True)
+                historytrading__lines__line_name=line_name,
+                historytrading__component__location__name__in=locations,
+                historytrading__component__component_type__name__in=component_types,
+                historytrading__component__machine_type__name__in=machine_types,
+                historytrading__issue_date__range=[date_start, date_end]
             ).annotate(
-                total_gi_qty=Coalesce(Sum('historytrading__gi_qty', filter=Q(historytrading__issue_date__date__range=[start_date, end_date])), Value(0)),
-                total_scrap_qty=Coalesce(Sum('historytrading__scrap_qty', filter=Q(historytrading__issue_date__date__range=[start_date, end_date])), Value(0)),
-                total_price=Coalesce(Sum(F('historytrading__gi_qty') * F('price'), filter=Q(historytrading__issue_date__date__range=[start_date, end_date])), Value(0)),
+                total_gi_qty=Coalesce(Sum('historytrading__gi_qty', filter=Q(historytrading__issue_date__date__range=[date_start, date_end])), Value(0)),
+                total_scrap_qty=Coalesce(Sum('historytrading__scrap_qty', filter=Q(historytrading__issue_date__date__range=[date_start, date_end])), Value(0)),
+                total_price=Coalesce(Sum(F('historytrading__gi_qty') * F('price'), filter=Q(historytrading__issue_date__date__range=[date_start, date_end])), Value(0)),
             )
 
             data = [
@@ -63,6 +77,8 @@ def data_analysis_breakdown(request):
                     "id": component.pk,
                     "name": component.name,
                     "model": component.model,
+                    "component_type": component.component_type.name,
+                    "machine_type": component.machine_type.name,
                     "unique_id": component.unique_id,
                     "price": component.price,
                     "location": component.location.name,
@@ -74,7 +90,7 @@ def data_analysis_breakdown(request):
                 for component in components
             ]
 
-            return Response({"detail": 'success', "data": data}, status=status.HTTP_200_OK)
+            return Response({"detail": 'success', "data": data, "date_range": f"{date_start.strftime('%m/%d/%Y')} to {date_end.strftime('%m/%d/%Y')}"}, status=status.HTTP_200_OK)
 
         else:
             return Response({"detail": 'date_mode data format is not correct'}, status=status.HTTP_409_CONFLICT)
@@ -90,135 +106,85 @@ def data_analysis_breakdown(request):
 def data_analysis_summary(request):
     try:
         lines = request.data.get('lines', [])
-        locations = request.data.get('locations',[])
-        component_types = request.data.get('component_types',[])
-        machine_types = request.data.get('machine_types',[])
-        equipments = request.data.get('equipments',[])
+        locations = request.data.get('locations', [])
+        component_types = request.data.get('component_types', [])
+        machine_types = request.data.get('machine_types', [])
         date_mode = request.data.get('date_mode')
         date_range = request.data.get('date_range')
 
-        query_main = Q(lines__line_name__in=lines) &\
-                Q(component__location__name__in=locations) &\
-                Q(component__component_type__name__in=component_types) &\
-                Q(component__machine_type__name__in=machine_types) # &\
-                # Q(component__name__in=equipments)
-        
-        start_date =  datetime.strptime(date_range['start'], '%m/%d/%Y').date()
-        end_date =  datetime.strptime(date_range['end'], '%m/%d/%Y').date()
+        start_date = datetime.strptime(date_range['start'], '%m/%d/%Y').date()
+        end_date = datetime.strptime(date_range['end'], '%m/%d/%Y').date()
 
         data_series = []
         date_labels = []
-        labelUpdated = False
+        label_updated = False
+
         for line in lines:
-            series_index = {}
-            series_index['name'] = line
-            series_index['gi_data'] = []
-            series_index['scrap_data'] = []
-            series_index['cost_data'] = []
-            series_index['data'] = []
+            series_data = {
+                'name': line,
+                'gi_data': [],
+                'scrap_data': [],
+                'cost_data': []
+            }
 
-            if date_mode == 'Days':
-                current_date = start_date
-                while current_date <= end_date:
+            current_date = start_date
+            date_increment = timedelta(days=1) if date_mode == 'Days' else None
 
-                    hObj = HistoryTrading.objects.filter(
-                        Q(lines__line_name=line) &\
-                        Q(component__location__name__in=locations) &\
-                        Q(component__component_type__name__in=component_types) &\
-                        Q(component__machine_type__name__in=machine_types) &\
-                        Q(issue_date__date=current_date)
-                    )
-                    gi = 0
-                    scrap = 0
-                    cost = 0
-                    for h in hObj:
-                        gi = gi + h.gi_qty
-                        scrap = scrap + h.scrap_qty
-                        cost = cost + (h.component.price * h.gi_qty)
-
-                    series_index['gi_data'].append(gi)
-                    series_index['scrap_data'].append(scrap)
-                    series_index['cost_data'].append(cost)
-
-                    if not labelUpdated : date_labels.append(current_date)
-                    current_date += timedelta(days=1)
-
-            elif date_mode == 'Months':
-                current_date = start_date.replace(day=1)  # Set the day to 1 to avoid inconsistencies
-                
-                while current_date <= end_date:
-                    # print(current_date, end_date)
-                    hObj = HistoryTrading.objects.filter(
-                        Q(lines__line_name=line) &\
-                        Q(component__location__name__in=locations) &\
-                        Q(component__component_type__name__in=component_types) &\
-                        Q(component__machine_type__name__in=machine_types) &\
-                        Q(issue_date__month=current_date.month)
-                    )
-                    gi = 0
-                    scrap = 0
-                    cost = 0
-                    for h in hObj:
-                        gi = gi + h.gi_qty
-                        scrap = scrap + h.scrap_qty
-                        cost = cost + (h.component.price * h.gi_qty)
-
-                    series_index['gi_data'].append(gi)
-                    series_index['scrap_data'].append(scrap)
-                    series_index['cost_data'].append(cost)
-
-                    if not labelUpdated : date_labels.append(datetime.strftime(current_date, '%Y-%m'))
-                    current_date += relativedelta(months=1)  # Move to the next month
-
+            if date_mode == 'Months':
+                current_date = start_date.replace(day=1)
+                date_increment = relativedelta(months=1)
             elif date_mode == 'Years':
-                current_date = start_date.replace(month=1, day=1)  # Set the date to the 1st of January of the start year
-                while current_date <= end_date:
-                    # print(current_date.year)  # You can replace this with your desired output or action
-                    if not labelUpdated : date_labels.append(current_date)
-                    series_index['data'].append(current_date)
+                current_date = start_date.replace(month=1, day=1)
+                date_increment = relativedelta(years=1)
 
-                    current_date += relativedelta(years=1)  # Move to the next yearate.year, end_date.year + 1): 
-            else:
-                return Response({"detail": 'date_mode data format is not correct'}, status=status.HTTP_409_CONFLICT)
-            
-            labelUpdated = True
-            data_series.append(series_index)
+            if not date_increment:
+                return Response({"detail": 'Invalid date_mode'}, status=status.HTTP_409_CONFLICT)
 
+            while current_date <= end_date:
+                date_filter = Q(issue_date__date__range=(start_date, end_date)) if date_mode == 'Days' \
+                    else Q(issue_date__month=current_date.month, issue_date__year=current_date.year)
 
-        data = {}
-        data['overall'] = {}
-        data['overall']['series'] = data_series
-        data['overall']['labels'] = date_labels
+                h_objects = HistoryTrading.objects.filter(
+                    Q(lines__line_name=line) &
+                    Q(component__location__name__in=locations) &
+                    Q(component__component_type__name__in=component_types) &
+                    Q(component__machine_type__name__in=machine_types) &
+                    date_filter
+                )
 
-        data['gi_total'] = {}
-        data['gi_total']["series"] = []
-        data['gi_total']["labels"] = []
-        data['scrap_total'] = {}
-        data['scrap_total']["series"] = []
-        data['scrap_total']["labels"] = []
-        data['cost_total'] = {}
-        data['cost_total']["series"] = []
-        data['cost_total']["labels"] = []
-        for s in data_series:
-            tt_gi_line = 0
-            tt_scrap_line = 0
-            tt_cost_line = 0
+                gi = sum(h.gi_qty for h in h_objects)
+                scrap = sum(h.scrap_qty for h in h_objects)
+                cost = sum(h.component.price * h.gi_qty for h in h_objects)
 
-            for gi_line in s["gi_data"]: tt_gi_line += gi_line
-            data['gi_total']["labels"].append(s["name"])
-            data['gi_total']["series"].append(tt_gi_line)
+                series_data['gi_data'].append(gi)
+                series_data['scrap_data'].append(scrap)
+                series_data['cost_data'].append(cost)
 
-            for scrap_line in s["scrap_data"]: tt_scrap_line += scrap_line
-            data['scrap_total']["labels"].append(s["name"])
-            data['scrap_total']["series"].append(tt_scrap_line)
+                if not label_updated:
+                    date_labels.append(current_date.strftime('%m/%d/%Y'))
+                
+                current_date += date_increment
 
-            for cost_line in s["cost_data"]: tt_cost_line += cost_line
-            data['cost_total']["labels"].append(s["name"])
-            data['cost_total']["series"].append(tt_cost_line)
+            label_updated = True
+            data_series.append(series_data)
 
+        data = {
+            'overall': {'series': data_series, 'labels': date_labels},
+            'gi_total': {'series': [], 'labels': []},
+            'scrap_total': {'series': [], 'labels': []},
+            'cost_total': {'series': [], 'labels': []},
+        }
+
+        for series in data_series:
+            data['gi_total']['labels'].append(series['name'])
+            data['gi_total']['series'].append(sum(series['gi_data']))
+
+            data['scrap_total']['labels'].append(series['name'])
+            data['scrap_total']['series'].append(sum(series['scrap_data']))
+
+            data['cost_total']['labels'].append(series['name'])
+            data['cost_total']['series'].append(sum(series['cost_data']))
 
         return Response({"detail": "success", "data": data}, status=status.HTTP_200_OK)
-
     except Exception as e:
-        print(str(e))
         return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
